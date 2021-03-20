1d36eafd82fe96a65fceed8f625789a49930cdde
Deepthi Koka <deepthi.koka94@gmail.com>
2021-03-20 15:58:56 +0530

    updated appclient id

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_aejhfBgYr', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '5ufik173bjnro40rn74luifvpl', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '2qngtdbqb72j749u6j3jo3v7kh', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
